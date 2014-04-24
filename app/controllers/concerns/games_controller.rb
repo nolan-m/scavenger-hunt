b3170ff639d53d8e1aff38748495b3d5b21b7b72
Nolan Miller & Corey Norris <student@epicodus.com>
2014-04-24 15:11:28 -0700

    create clues for each game

--- app/controllers/concerns/games_controller.rb
+++ app/controllers/concerns/games_controller.rb
@@ -8,6 +8,9 @@ class GamesController < ApplicationController
     @location = Location.create(latitude: separated_coords[0], longitude: separated_coords[1])
     @game = @location.games.new(game_params)
     if @game.save
+      params[:clues].each do |clue|
+        @game.clues.create(:content => clue)
+      end
       flash[:notice] = "Game created"
       redirect_to root_url
     else
